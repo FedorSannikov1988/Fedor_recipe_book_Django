70a4e226ef62ec3e5ee3937b4ff4fcca853eafa6
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2024-02-06 22:28:36 +0300

    fix bug 7 and work on admin

--- fedor_recipe_book/recipe_book/admin.py
+++ fedor_recipe_book/recipe_book/admin.py
@@ -3,6 +3,15 @@ from recipe_book.models import Recipes, \
                                RecipeCategories, \
                                CommentsOnRecipe
 
-admin.site.register(Recipes)
+
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    #ordering = ['birthday']
+    #list_filter = ['birthday']
+    #search_fields = ['email']
+    #search_help_text = 'Поиск по полю email'
+
+
+admin.site.register(Recipes, RecipesAdmin)
 admin.site.register(RecipeCategories)
 admin.site.register(CommentsOnRecipe)
--- fedor_recipe_book/users/admin.py
+++ fedor_recipe_book/users/admin.py
@@ -2,5 +2,16 @@ from users.models import Users
 from django.contrib import admin
 
 
-admin.site.register(Users)
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ["username", "first_name", "last_name",
+                    "is_superuser", "is_staff", "date_joined"]
+    #list_display = ['image', 'email', 'gender', 'birthday', 'is_superuser', 'is_staff']
+    #ordering = ['birthday']
+    #list_filter = ['birthday']
+    #search_fields = ['email']
+    #search_help_text = 'Поиск по полю email'
+
+
+#admin.site.register(Users, UsersAdmin)
 
