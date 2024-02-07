0eb11efe23de2101a7eabb967bf03dfeb540630a
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2024-02-07 09:25:27 +0300

    changes for placement 2

--- fedor_recipe_book/fedor_recipe_book/settings.py
+++ fedor_recipe_book/fedor_recipe_book/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY: str = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
@@ -109,26 +109,26 @@ WSGI_APPLICATION = 'fedor_recipe_book.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'FedorSannikov198$default',
-#        'USER': 'FedorSannikov198',
-#        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-#        'HOST': 'FedorSannikov1988.mysql.pythonanywhere-services.com',
-#        'OPTIONS': {
-#            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
-#            'charset': 'utf8mb4',
-#        },
-#    }
-#}
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'FedorSannikov198$default',
+       'USER': 'FedorSannikov198',
+       'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+       'HOST': 'FedorSannikov1988.mysql.pythonanywhere-services.com',
+       'OPTIONS': {
+           'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+           'charset': 'utf8mb4',
+       },
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
